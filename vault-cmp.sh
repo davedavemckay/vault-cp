ebc62e305d0b7db28fadd789a43cd478bf566fc5
Dave McKay <d.mckay@epcc.ed.ac.uk>
2025-04-07 13:26:40 +0100

    remove mount

--- vault-cmp.sh
+++ vault-cmp.sh
@@ -4,7 +4,7 @@
 for i in $(vault kv list $1 | tail -n +3)
 do
     orig=$(vault kv get $2$i | grep -A 10 '=== Data ===')
-    new=$(vault kv get -mount=kv-v2 $1$i | grep -A 10 '=== Data ===')
+    new=$(vault kv get $1$i | grep -A 10 '=== Data ===')
     if [[ $orig == $new ]]
     then
         echo $i matches
